27c268fa532ff2bd78573760eff1ccc3b5d07ae8
Christian Koestlin <christian.koestlin@gmail.com>
2021-12-28 22:05:00 +0100

    Download namespaced openvpn

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,5 @@
-require 'sshkit'
-require 'sshkit/dsl'
+require "sshkit"
+require "sshkit/dsl"
 include SSHKit::DSL
 
 def server(host_name, roles, location)
@@ -10,8 +10,9 @@ def server(host_name, roles, location)
 end
 
 servers = [
-  server("fs.local", [:torrent], :munich),
-  server("seehaus-piano.local", [:torrent], :seehaus),
+  server("fs.local", [:torrent, :apt], :munich),
+  server("slideshow.local", [:slideshow, :apt], :munich),
+  server("seehaus-piano.local", [:torrent, :apt], :seehaus),
 ]
 class Array
   def with_role(role)
@@ -22,13 +23,33 @@ class Array
   end
 end
 
-def exe(ctx, command)
-  ctx.execute(command, interaction_handler: AllOutputInteractionHandler.new)
+def exe(ctx, command, sudo=false)
+  ctx.execute("#{with_sudo(sudo)}#{command}", interaction_handler: AllOutputInteractionHandler.new)
+end
+
+def with_sudo(sudo)
+  return sudo ? "sudo " : ""
+end
+
+def install_from_web(ctx, url, destination)
+  if  ctx.test("[ -f #{destination} ]")
+    remote_checksum = ctx.capture("sudo sha512sum #{destination}").split(" ").first
+    system("curl --silent --output tmp.f #{url}")
+    local_checksum = `sha512sum tmp.f`.strip.split(" ").first
+    if remote_checksum == local_checksum
+      ctx.info("#{destination} is already up2date")
+      return false
+    end
+  end
+  exe(ctx, "curl --silent --output #{destination} #{url}")
+  exe(ctx, "sudo chown root:root #{destination}")
+  exe(ctx, "sudo chmod 766 #{destination}")
+  return true
 end
 
 def upload(ctx, file, destination, user, group, mask, sudo=false)
   if ctx.test("[ -f #{destination} ]")
-    remote_checksum = ctx.capture("#{sudo ? 'sudo ':''}sha512sum #{destination}").split(" ").first
+    remote_checksum = ctx.capture("#{with_sudo(sudo)}sha512sum #{destination}").split(" ").first
     local_checksum = `sha512sum #{file}`.strip.split(" ").first
     if remote_checksum == local_checksum
       ctx.info("#{destination} is already up2date")
@@ -46,7 +67,7 @@ end
 def upload_encrypted_file(ctx, file, destination, user, group, mask, sudo=false)
   decrypted = `gpg --decrypt --quiet #{file}`
   if ctx.test("[ -f #{destination} ]")
-    remote_checksum = ctx.capture("#{sudo ? 'sudo ':''}sha512sum #{destination}").split(" ").first
+    remote_checksum = ctx.capture("#{with_sudo(sudo)}sha512sum #{destination}").split(" ").first
     local_checksum = `gpg --decrypt --quiet #{file} | sha512sum`.strip.split(" ").first
     if remote_checksum == local_checksum
       info("#{destination} is already up2date")
@@ -92,7 +113,7 @@ class Service
   end
   def enable
     if enabled?
-      @ctx.info("Service is already enabled")
+      @ctx.info("Service #{@name} is already enabled")
       return
     end
     exe(@ctx, "sudo systemctl enable #{@name}")
@@ -103,12 +124,44 @@ class Service
   end
 end
 
-on servers.with_role(:torrent).in(:munich) do |host|
-  info("Installing openvpn + deluge in namespace on #{host}")
-  Service.new(self, "openvpn-in-namespace-client@italy", "openvpn-in-namespace-client@italy/openvpn-in-namespace-client@.service").install
-  upload_encrypted_file(self, "openvpn-in-namespace-client@italy/pia.pass.gpg", "/etc/openvpn/client/pia.pass", "root", "root", "400", true)
-  upload_encrypted_file(self, "openvpn-in-namespace-client@italy/italy.conf.gpg", "/etc/openvpn/client/italy.conf", "root", "root", "400", true)
-  upload(self, "openvpn-in-namespace-client@italy/up.sh", "/etc/openvpn/client/up.sh", "root", "root", "700", true)
-end
+locations =
+  [
+    :munich,
+    :seehaus,
+  ]
 
+locations.each do |location|
+  namespace location do
+    desc "Run all in #{location}"
+    all = task :all do
+    end
+
+    desc "Configure torrent servers for #{location}"
+    t = task :torrent do
+      on servers.with_role(:torrent).in(location) do |host|
+        info("Installing openvpn + deluge in namespace on #{host}")
+
+        install_from_web(self, "https://raw.githubusercontent.com/slingamn/namespaced-openvpn/master/namespaced-openvpn", "/usr/local/bin/namespaced-openvpn")
+        Service.new(self, "openvpn-in-namespace-client@italy", "openvpn-in-namespace-client@italy/openvpn-in-namespace-client@.service").install
+
+        info("Installing custom openvpn config and scripts on #{host}")
+        upload_encrypted_file(self, "openvpn-in-namespace-client@italy/pia.pass.gpg", "/etc/openvpn/client/pia.pass", "root", "root", "400", true)
+        upload_encrypted_file(self, "openvpn-in-namespace-client@italy/italy.conf.gpg", "/etc/openvpn/client/italy.conf", "root", "root", "400", true)
+        upload(self, "openvpn-in-namespace-client@italy/up.sh", "/etc/openvpn/client/up.sh", "root", "root", "700", true)
+      end
+    end
+    all.enhance([t])
+
+    desc "Update apt based servers"
+    t = task :update do
+      on servers.with_role(:apt).in(location) do |host|
+        info("Updating #{host}")
+        exe(self, "apt update", true)
+        exe(self, "apt dist-upgrade", true)
+        exe(self, "apt autoremove", true)
+      end
+    end
+    all.enhance([t])
+  end
+end
 task :default
